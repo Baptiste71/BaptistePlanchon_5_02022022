fe8e9fce209cf7f4bb3c457d5281cb1d9af77700
Baptiste71 <planchonbaptiste@gmail.com>
2022-02-12 10:58:20 +0100

    importation GET du serveur 3000

---
+++ js/index.js
@@ -0,0 +1,33 @@
+products();
+
+function products() {
+    allProducts();
+}
+
+function allProducts() {
+    fetch("http://localhost:3000/api/products")
+        .then(function(res) {
+            if (res.ok) {
+              return res.json();
+            }
+        })
+
+        .then(function(value) {           
+            console.log(value);
+        })
+        
+        .catch(function(err) {
+            //Une erreur est survenue
+        });
+        
+        
+
+
+
+
+
+
+
+}    
+
+
