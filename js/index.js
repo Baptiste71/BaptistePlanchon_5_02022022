a9054e104ed0a26c2726b750258db3757971cc97
Baptiste71 <planchonbaptiste@gmail.com>
2022-02-14 19:05:47 +0100

    appel des produits

--- js/index.js
+++ js/index.js
@@ -1,33 +1,31 @@
-products();
+const productsSection = document.getElementById("items");
 
-function products() {
-    allProducts();
-}
+allProducts();
 
 function allProducts() {
-    fetch("http://localhost:3000/api/products")
-        .then(function(res) {
-            if (res.ok) {
-              return res.json();
-            }
-        })
-
-        .then(function(value) {           
-            console.log(value);
-        })
-        
-        .catch(function(err) {
-            //Une erreur est survenue
-        });
-        
-        
-
-
-
-
-
-
-
-}    
-
-
+  fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error(res.statusText);
+    })
+    .then(function (values) {
+      let productsHtml = "";
+      for (let value of values) {
+        productsHtml += `
+                    <a href="./product.html?id=${value._id}">
+                        <article>
+                        <img src=${value.imageUrl} alt=${value.altTxt}>
+                        <h3 class="productName">${value.name}</h3>
+                        <p class="productDescription">${value.description}</p>
+                        </article>
+                    </a>
+                    `;
+      }
+      productsSection.insertAdjacentHTML("afterbegin", productsHtml);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+}
