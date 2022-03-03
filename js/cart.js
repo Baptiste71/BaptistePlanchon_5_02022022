644eaed1e5628a25db4e6f0ba0d103eba82f6b41
Baptiste71 <planchonbaptiste@gmail.com>
2022-03-03 18:48:37 +0100

    mise en place des fonctions du panier à retoucher

--- js/cart.js
+++ js/cart.js
@@ -28,9 +28,9 @@ if (cartLocalStorage === null) {
                             <div class="cart__item__content__description">
                               <h2>${value.name}</h2>
                               <p>${productInLocalStorage.color}</p>
-                              <p>${value.price * productInLocalStorage.quantity}</p>                     
+                              <p>${value.price * productInLocalStorage.quantity}€</p>                     
                             </div>
-                            <div class="cart__item__content__settings">
+                              <div class="cart__item__content__settings">
                               <div class="cart__item__content__settings__quantity">
                                 <p>Qté : </p>
                                 <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productInLocalStorage.quantity}">
@@ -45,4 +45,46 @@ if (cartLocalStorage === null) {
         cartFull.insertAdjacentHTML("afterbegin", productsInCart);
       });
   }
+}
+
+// Prise en compte de la modification de la quantité sur la page Panier
+
+const quantityValueInInput = document.querySelector(".cart__item__content__settings__quantity");
+const quantityChoice = document.querySelector(".itemQuantity");
+let inputValue = cartLocalStorage;
+
+if (inputValue) {
+  let indexFind = false;
+  for (let [index, productInCart] of inputValue.entries()) {
+    if (productInCart.quantity != quantityChoice) {
+      indexFind = true;
+
+      if (inputValue[index].quantity + parseInt(quantityChoice) <= 100) {
+        inputValue[index].quantity += parseInt(quantityChoice);
+      }
+      if (indexFind) {
+        localStorage.setItem(".cart__item__content__settings__quantity", JSON.stringify(inputValue));
+      } else {
+        inputValue.push(quantityChoice);
+        localStorage.setItem(".cart__item__content__settings__quantity", JSON.stringify(inputValue));
+      }
+    }
+  }
+}
+
+// Suppression d'élément dans le panier
+
+let deleteItem = document.querySelectorAll(".deleteItem");
+console.log(deleteItem);
+
+for (let deleteItemInCart = [cartLocalStorage]; deleteItemInCart < [deleteItem.length]; deleteItemInCart++) {
+  deleteItem[deleteItemInCart].addEventListener("click", (events) => {
+    events.preventDefault();
+
+    let deleteItemId = cartLocalStorage[deleteItemInCart].idSelectProduct;
+    console.log();
+
+    cartLocalStorage = cartLocalStorage.filter((el) => el.idSelectProduct !== deleteItemId);
+    console.log(cartLocalStorage);
+  });
 }
