b08bbefaf766d50c8686d77b0363c40e1375315c
Baptiste71 <planchonbaptiste@gmail.com>
2022-03-10 20:00:02 +0100

    creation fonctions +  prix total du panier

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
 const cartFull = document.getElementById("cart__items");
 const totalArticleInCart = document.getElementById("totalQuantity");
 const totalPriceOfArticle = document.getElementById("totalPrice");
+const rules = /^[a-zA-Z-\s]+$/;
 
 // Recuperation des données du localStorage
 
@@ -44,125 +45,144 @@ async function startUp() {
     cartFull.insertAdjacentHTML("afterbegin", productsInCart);
 
     // modification de la quantité dans la page panier
+    addArticleOnLocalStorage();
 
-    const addArticleInCart = document.querySelectorAll(".cart__item__content__settings__quantity");
+    function addArticleOnLocalStorage() {
+      const addArticleInCart = document.querySelectorAll(".cart__item__content__settings__quantity");
 
-    addArticleInCart.forEach((elem) => {
-      elem.addEventListener("change", (event) => {
-        let canapFinded = event.target.closest("article");
+      addArticleInCart.forEach((elem) => {
+        elem.addEventListener("change", (event) => {
+          let canapFinded = event.target.closest("article");
 
-        if (cartLocalStorage) {
-          let indexFind;
+          if (cartLocalStorage) {
+            let indexFind;
 
-          indexFind = cartLocalStorage.findIndex((savedProduct) => {
-            return savedProduct.id === canapFinded.getAttribute("data-id") && savedProduct.color === canapFinded.getAttribute("data-color");
-          });
+            indexFind = cartLocalStorage.findIndex((savedProduct) => {
+              return savedProduct.id === canapFinded.getAttribute("data-id") && savedProduct.color === canapFinded.getAttribute("data-color");
+            });
 
-          if (parseInt(event.target.value) > 0 && parseInt(event.target.value) <= 100) {
-            cartLocalStorage[indexFind].quantity = parseInt(event.target.value);
-            localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
+            if (parseInt(event.target.value) > 0 && parseInt(event.target.value) <= 100) {
+              cartLocalStorage[indexFind].quantity = parseInt(event.target.value);
+              localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
+            }
           }
-        }
+        });
       });
-    });
+    }
 
     // Suppression d'élément dans le panier
+    deleteArticleOnLocalStorage();
 
-    const deleteProduct = document.querySelectorAll(".deleteItem");
+    function deleteArticleOnLocalStorage() {
+      const deleteProduct = document.querySelectorAll(".deleteItem");
 
-    deleteProduct.forEach((elem) => {
-      elem.addEventListener("click", (event) => {
-        let canapFinded = event.target.closest("article");
+      deleteProduct.forEach((elem) => {
+        elem.addEventListener("click", (event) => {
+          let canapFinded = event.target.closest("article");
 
-        if (cartLocalStorage) {
-          let indexFind;
+          if (cartLocalStorage) {
+            let indexFind;
 
-          indexFind = cartLocalStorage.findIndex((savedProduct) => {
-            return savedProduct.id === canapFinded.getAttribute("data-id") && savedProduct.color === canapFinded.getAttribute("data-color");
-          });
+            indexFind = cartLocalStorage.findIndex((savedProduct) => {
+              return savedProduct.id === canapFinded.getAttribute("data-id") && savedProduct.color === canapFinded.getAttribute("data-color");
+            });
 
-          cartLocalStorage.splice(indexFind, 1);
-          canapFinded.remove();
-          localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
-        }
+            cartLocalStorage.splice(indexFind, 1);
+            canapFinded.remove();
+            localStorage.setItem("cart", JSON.stringify(cartLocalStorage));
+          }
+        });
       });
-    });
-    // Total du prix dans le panier
-
-    const articlePrice = document.querySelector(".cart__item__content__description ");
-
-    let priceArticle = [];
-
-    for (let products = 0; products < cartLocalStorage.length; products++) break;
-    {
-      //let totalPrice = articlePrice.lastElementChild.textContent;
-      priceArticle.push(totalPrice);
-      console.log(totalPrice);
     }
-
-    const calcul = (total, valueCurrent) => total + valueCurrent;
-    const totalOnBill = priceArticle.reduce(calcul);
-    console.log(totalOnBill);
-
-    const totalPriceHTML = `<span id="totalPrice">${totalOnBill}</span>`;
-    totalPriceOfArticle.insertAdjacentHTML("afterbegin", totalPriceHTML);
 
     // Total des articles dans le panier
+    totalQuantityInLocalStorage();
 
-    let totalQuantity = [];
+    function totalQuantityInLocalStorage() {
+      let totalQuantity = [];
 
-    for (let article = 0; article < cartLocalStorage.length; article++) {
-      let quantityOfArticleInCart = cartLocalStorage[article].quantity;
+      for (let article = 0; article < cartLocalStorage.length; article++) {
+        let quantityOfArticleInCart = cartLocalStorage[article].quantity;
 
-      totalQuantity.push(quantityOfArticleInCart);
-      console.log(totalQuantity);
+        totalQuantity.push(quantityOfArticleInCart);
+        console.log(totalQuantity);
+      }
+
+      const reducer = (addition, currentValue) => addition + currentValue;
+      const totalArticle = totalQuantity.reduce(reducer, 0);
+      console.log(totalArticle);
+
+      const totalQuantityHTML = `<span id="totalQuantity">${totalArticle}</span>`;
+      totalArticleInCart.insertAdjacentHTML("afterbegin", totalQuantityHTML);
     }
 
-    const reducer = (addition, currentValue) => addition + currentValue;
-    const totalArticle = totalQuantity.reduce(reducer, 0);
-    console.log(totalArticle);
+    // Total du prix dans le panier
 
-    const totalQuantityHTML = `<span id="totalQuantity">${totalArticle}</span>`;
-    totalArticleInCart.insertAdjacentHTML("afterbegin", totalQuantityHTML);
-  } // Confirmation du formulaire
+    totalPriceOnBill();
 
-  const setForm = document.querySelector(".cart__order__form__submit");
-  const firstName = document.getElementById("firstName").value == "";
-  const errorFirstName = document.getElementById("firstNameErrorMsg");
-  const lastName = document.getElementById("lastName").value == "";
-  const errorLastName = document.getElementById("lastNameErrorMsg");
-  const address = document.getElementById("address").value == "";
-  const errorAddress = document.getElementById("addressErrorMsg");
-  const city = document.getElementById("city").value == "";
-  const errorCity = document.getElementById("cityErrorMsg");
-  const email = document.getElementById("email").value == "";
-  const errorEmail = document.getElementById("emailErrorMsg");
+    async function totalPriceOnBill() {
+      let totalPrice = [];
+      for await (let productInLocalStorage of cartLocalStorage) {
+        const res = await fetch(`http://localhost:3000/api/products/${productInLocalStorage.id}`);
+        if (res.ok) {
+          const resJson = await res.json();
 
-  setForm.addEventListener("submit", (event) => {
-    if (firstName.value || lastName.value || address.value || city.value || email.value) {
-      errorFirstName.innerHTML = "Merci de renseigner ce champ !";
-      errorLastName.innerHTML = "Merci de renseigner ce champ !";
-      errorAddress.innerHTML = "Merci de renseigner ce champ !";
-      errorCity.innerHTML = "Merci de renseigner ce champ !";
-      errorEmail.innerHTML = "Merci de renseigner ce champ !";
-
-      event.preventDefault();
+          for (let article = 0; cartLocalStorage.length; article++) break;
+          {
+            let totalPriceOfCart = resJson.price * productInLocalStorage.quantity;
+            totalPrice.push(totalPriceOfCart);
+            console.log(totalPrice);
+          }
+        }
+      }
+      const reducer = (addition, currentValue) => addition + currentValue;
+      const totalArticle = totalPrice.reduce(reducer, 0);
+      console.log(totalArticle);
+      const totalPriceHTML = `<span id="totalPrice">${totalArticle}</span>`;
+      totalPriceOfArticle.insertAdjacentHTML("afterbegin", totalPriceHTML);
     }
+  }
 
-    const order = [];
-    order.push(cartLocalStorage);
+  // Confirmation du formulaire
 
-    const buyProduct = {
-      productsOrder: order,
+  formShopper();
 
-      shopperInformation: {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        address: address.value,
-        city: city.value,
-        email: email.value,
-      },
-    };
-    console.log(buyProduct);
-  });
+  function formShopper() {
+    const setForm = document.querySelector(".cart__order__form");
+    const firstName = document.getElementById("firstName").value === "".rules;
+    const errorFirstName = document.getElementById("firstNameErrorMsg");
+    const lastName = document.getElementById("lastName").value === "".rules;
+    const errorLastName = document.getElementById("lastNameErrorMsg");
+    const address = document.getElementById("address").value === "";
+    const errorAddress = document.getElementById("addressErrorMsg");
+    const city = document.getElementById("city").value === "";
+    const errorCity = document.getElementById("cityErrorMsg");
+    const email = document.getElementById("email").value === "";
+    const errorEmail = document.getElementById("emailErrorMsg");
+
+    setForm.addEventListener("submit", (event) => {
+      if (firstName.value || lastName.value || address.value || city.value || email.value == "") {
+        errorFirstName.innerHTML = "Merci de renseigner ce champ !";
+        errorLastName.innerHTML = "Merci de renseigner ce champ !";
+        errorAddress.innerHTML = "Merci de renseigner ce champ !";
+        errorCity.innerHTML = "Merci de renseigner ce champ !";
+        errorEmail.innerHTML = "Merci de renseigner ce champ par une adresse mail valide !";
+
+        event.preventDefault();
+      } else if (rules.test(firstName.value || lastName.value || address.value || city.value || email.value) == false) {
+        errorFirstName.innerHTML = "Merci de renseigner ce champ !";
+        errorLastName.innerHTML = "Merci de renseigner ce champ !";
+        errorAddress.innerHTML = "Merci de renseigner ce champ !";
+        errorCity.innerHTML = "Merci de renseigner ce champ !";
+        errorEmail.innerHTML = "Merci de renseigner ce champ par une adresse mail valide !";
+
+        event.preventDefault();
+      }
+      const order = [];
+      order.push(cartLocalStorage);
+      console.log(order);
+
+      const info = {};
+    });
+  }
 }
