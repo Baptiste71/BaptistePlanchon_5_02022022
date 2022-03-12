540da702e5dfd6850bff08062f3164c8f947ce63
Baptiste71 <planchonbaptiste@gmail.com>
2022-03-12 09:33:17 +0100

    confirmation de commande

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,3 +1,13 @@
+const numberOfOrder = document.getElementById("orderId");
+
 let cartLocalStorage = JSON.parse(localStorage.getItem("orderId"));
 
 console.log(cartLocalStorage);
+
+orderConfirmation();
+
+function orderConfirmation() {
+  let orderIdNumberHTML = `<span id="orderId">${cartLocalStorage.value}</span>`;
+
+  numberOfOrder.insertAdjacentHTML(`afterbegin`, orderIdNumberHTML);
+}
